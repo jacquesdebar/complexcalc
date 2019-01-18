1d193e202d1f84a178efd60176820f3b2f59650e
Jacques DeBar <jacques.debar@gmail.com>
2019-01-17 16:36:48 -0800

    define add and multiply functions

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,36 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    
+    func add(lhs: Int, rhs: Int) -> Int {
+        return (lhs + rhs)
+    }
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return (lhs - rhs)
+    }
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return (lhs * rhs)
+    }
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return (lhs / rhs)
+    }
+    
+    
+    func add(_ arr: [Int]) -> Int {
+        var ans = 0
+        for n in arr {
+            ans += n
+        }
+        return ans
+    }
+    func multiply(_ arr: [Int]) -> Int {
+        var ans = 1
+        for n in arr {
+            ans *= n
+        }
+        return ans
+    }
+    
+    
+    
 }
