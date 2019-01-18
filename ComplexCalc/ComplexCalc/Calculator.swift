21357aa38e6b754a4c93a2b1b7e4b3f964cba0ae
Jacques DeBar <jacques.debar@gmail.com>
2019-01-17 20:16:14 -0800

    define mathOp functions

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -11,22 +11,36 @@ import Foundation
 // All your work will go in here
 class Calculator {
     
+    //assert(calc.add(lhs: 2, rhs: 2) == 4)
     func add(lhs: Int, rhs: Int) -> Int {
         return (lhs + rhs)
     }
+    
+    //assert(calc.subtract(lhs: 2, rhs: 2) == 0)
     func subtract(lhs: Int, rhs: Int) -> Int {
         return (lhs - rhs)
     }
+    
+    //assert(calc.multiply(lhs: 2, rhs: 2) == 4)
     func multiply(lhs: Int, rhs: Int) -> Int {
         return (lhs * rhs)
     }
+    
+    //assert(calc.divide(lhs: 2, rhs: 2) == 1)
     func divide(lhs: Int, rhs: Int) -> Int {
         return (lhs / rhs)
     }
     
     
     
+    //assert(calc.mathOp(lhs: 5, rhs: 5, op: { ($0 + $1) + ($0 * $1) }) == 35)
+    //assert(calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20)
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
     
+    
+    //assert(calc.add([1, 2, 3, 4, 5]) == 15)
     func add(_ arr: [Int]) -> Int {
         var ans = 0
         for n in arr {
@@ -34,6 +48,8 @@ class Calculator {
         }
         return ans
     }
+    
+    //assert(calc.multiply([1, 2, 3, 4, 5]) == 120)
     func multiply(_ arr: [Int]) -> Int {
         var ans = 1
         switch arr.count {
@@ -47,10 +63,16 @@ class Calculator {
         return ans
     }
     
+    
+    //assert(calc.count([1, 2, 3, 4, 5, 6, 7, 8]) == 8)
+    //assert(calc.count([]) == 0)
     func count(_ arr: [Int]) -> Int {
         return arr.count
     }
-
+    
+    //assert(calc.avg([2, 2, 2, 2, 2, 2]) == 2)
+    //assert(calc.avg([1, 2, 3, 4, 5]) == 3)
+    //assert(calc.avg([1]) == 1)
     func avg(_ arr: [Int]) -> Int {
         var total = 0
         let nums = arr.count
@@ -61,17 +83,16 @@ class Calculator {
         return ans
     }
     
-    
-    
-    
-    
     //assert(calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6)
     //    // this is (((0 op 1) op 2) op 3)
     //assert(calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15)
     //    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
     //assert(calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1)
     //    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
-    //
+    func mathOp (args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        return args.reduce(beg, op)
+    }
+    
     //let p1 = (5, 5)
     //let p2 = (12, -27)
     //let p3 = (-4, 4)
@@ -85,6 +106,10 @@ class Calculator {
     //let pd2 = ["x": -4, "y": 4]
     //assert(calc.add(lhs: pd1, rhs: pd2) == ["x": 1, "y": 9])
     //assert(calc.subtract(lhs: pd1, rhs: pd2) == ["x": 9, "y": 1])
+    
+    
+    
+    
 
     
     
